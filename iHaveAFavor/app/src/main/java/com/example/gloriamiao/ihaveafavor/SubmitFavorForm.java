831c8ceea2d4cd77023dbb15d9d287da23619e4d
Charles Thorson <charlesthorson@Charless-MacBook-Pro-2.local>
2016-11-12 22:12:08 -0800

    clock

--- iHaveAFavor/app/src/main/java/com/example/gloriamiao/ihaveafavor/SubmitFavorForm.java
+++ iHaveAFavor/app/src/main/java/com/example/gloriamiao/ihaveafavor/SubmitFavorForm.java
@@ -1,16 +1,22 @@
 package com.example.gloriamiao.ihaveafavor;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.format.DateFormat;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.gloriamiao.ihaveafavor.ItemListActivity;
 import com.example.gloriamiao.ihaveafavor.R;
@@ -25,6 +31,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SubmitFavorForm extends AppCompatActivity {
+
+    static int chosenHour = 0;
+    static int chosenMin = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +51,35 @@ public class SubmitFavorForm extends AppCompatActivity {
             }
         });
     }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            chosenHour = hourOfDay;
+            chosenMin = minute;
+        }
+    }
+
     public void selectLocation(View view){
         //Use your geolocation stuff here
         TextView mTextView = (TextView) findViewById(R.id.place_message);
         mTextView.setText("text here");
     }
-    public void submitForm(View view){
+    public void submitFavor(View view){
         EditText favor = (EditText) findViewById(R.id.favor_message);
         TextView place = (TextView) findViewById(R.id.place_message);
         TextView time = (TextView) findViewById(R.id.time_message);
@@ -59,6 +91,8 @@ public class SubmitFavorForm extends AppCompatActivity {
         testObject.put("favor", f);
         testObject.put("place", p);
         testObject.put("time", t);
+        testObject.put("hour", chosenHour);
+        testObject.put("minute", chosenMin);
         testObject.put("requester", requester);
         testObject.put("accepted", false);
         testObject.saveInBackground();
@@ -88,6 +122,11 @@ public class SubmitFavorForm extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
 
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 
     public void destroyWindow(View view){
